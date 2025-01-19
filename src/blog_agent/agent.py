be04f09192f600804ac1814d1672c0b701629a7b
happy <168318776+sad-zero@users.noreply.github.com>
2025-01-19 21:01:21 +0900

    Task: Change I/O Specification (#4)

--- src/blog_agent/agent.py
+++ src/blog_agent/agent.py
@@ -1,20 +1,73 @@
-from typing import TypedDict
+from typing import Self, TypedDict
 
 from langchain.prompts import ChatPromptTemplate
-from langchain_core.runnables import RunnablePassthrough
 from langchain_openai import ChatOpenAI
+from pydantic import BaseModel, Field
 
 
-def write_post(title: str, restaurant: str, review: str, max_length: int) -> str:
+class PostGuide(BaseModel):
+    title: str
+    review: str
+    max_length: int
+    keywords: list[str]
+    foods: list[str]
+    restaurant: str | None = Field(default=None)
+
+    def with_restaurant(self, restaurant: str) -> Self:
+        if not isinstance(restaurant, str):
+            err_msg: str = f"restaurant is not str: {restaurant}"
+            raise TypeError(err_msg)
+        return PostGuide(
+            title=self.title,
+            review=self.review,
+            max_length=self.max_length,
+            keywords=self.keywords,
+            foods=self.foods,
+            restaurant=restaurant,
+        )
+
+
+def find_restaurant(title: str) -> str:
+    class Response(TypedDict):
+        restaurant: str
+
+    system_prompt = """
+Please find a restaurant's name in the given title.
+---
+Please response as JSON and follow schema below(defined by Python).
+class Response(TypedDict):
+    restaurant: str
+---
+Please do your best. Let's start!
+""".strip()
+    human_prompt = """
+Title is here.
+{title}
+""".strip()
+    template = ChatPromptTemplate.from_messages(
+        [
+            ("system", system_prompt),
+            ("human", human_prompt),
+        ]
+    )
+    llm = ChatOpenAI(model="gpt-4o-mini", temperature=0.52, max_completion_tokens=100)
+    chain = template | llm.with_structured_output(Response, method="json_schema")
+    response: Response = chain.invoke({"title": title})
+    return response["restaurant"]
+
+
+def write_post(post_guide: PostGuide) -> str:
     system_prompt = """
 As a food blog writer, your task is to write a post based on user's request.
 ---
 Please follow these guidelines.
-- You should start with "안녕하세요, 오늘 소개해드릴 곳은 {restaurant}입니다!"
 - You should write an attractive post.
-- The post's word count should be less than {max_length}.
-- You should end with sentences to recommend visiting {restaurant}.
 - You should write the post in korean.
+- You should start with "안녕하세요, 오늘 소개해드릴 곳은 {restaurant}입니다!"
+- The post's word count should be less than {max_length}.
+- You should use given keywords in every 300 words as the context.
+- You should end with sentences about visiting {restaurant}.
+- You must not use recommendation sentences like "추천합니다".
 ---
 Please consider these LLM configurations.
 - temperature: 0.52
@@ -33,22 +86,21 @@ Food's review is here.
 ---
 Post's maximum lenght is here.
 {max_length}
+---
+Keywords are here.
+{keywords}
+---
+Eaten foods are here.
+{foods}
 """.strip()
     template = ChatPromptTemplate.from_messages([("system", system_prompt), ("human", human_prompt)])
-    llm = ChatOpenAI(model="gpt-4o-2024-11-20", temperature=0.52, max_completion_tokens=1500)
+    llm = ChatOpenAI(model="gpt-4o-2024-11-20", temperature=0.52, max_completion_tokens=2000)
     chain = template | llm
-    res = chain.invoke(
-        {
-            "title": title,
-            "restaurant": restaurant,
-            "review": review,
-            "max_length": max_length,
-        }
-    )
+    res = chain.invoke(post_guide.model_dump())
     return res.content
 
 
-def write_hashtags(post: str) -> list[str]:
+def write_hashtags(post: str, post_guide: PostGuide) -> list[str]:
     class Response(TypedDict):
         hashtags: list[str]
 
@@ -56,7 +108,7 @@ def write_hashtags(post: str) -> list[str]:
 As a blog writer, your task is to write hashtags related to the user's post.
 ---
 Please follow these guidelines.
-- The number of hashtags should be equal to 20.
+- The number of hashtags should be equal to {number}.
 - You should write hashtags aligning with user's post.
 - You should write the post in korean.
 ---
@@ -81,6 +133,9 @@ Post is here.
         ]
     )
     llm = ChatOpenAI(model="gpt-4o-2024-11-20", temperature=0.52, max_completion_tokens=500)
-    chain = {"post": RunnablePassthrough()} | template | llm.with_structured_output(Response, method="json_schema")
-    response: Response = chain.invoke(post)
-    return response["hashtags"]
+    chain = template | llm.with_structured_output(Response, method="json_schema")
+    if number := max(0, 20 - len(post_guide.keywords)):
+        response: Response = chain.invoke({"post": post, "number": number})
+    hashtags: list[str] = response["hashtags"]
+    keyword_hashtags: list[str] = ["#" + keyword.strip("# ") for keyword in post_guide.keywords[: 20 - len(hashtags)]]
+    return hashtags + keyword_hashtags
--- src/blog_agent/web.py
+++ src/blog_agent/web.py
@@ -1,20 +1,36 @@
 import streamlit as st
 
-from blog_agent.agent import write_hashtags, write_post
+from blog_agent.agent import PostGuide, find_restaurant, write_hashtags, write_post
 
 title = st.text_input("Enter title")
-restaurant = st.text_input("Enter restaurant")
 review = st.text_area("Enter review")
-max_length = st.number_input("Enter post's length", min_value=100, max_value=1000)
+max_length = st.number_input("Enter post's length", min_value=500, max_value=2000)
+keywords: list[str] | str = st.text_input("Enter comma-seperated keywords. Example) 맛집,서울")
+keywords = [keyword.strip() for keyword in keywords.split(",")]
+foods: list[str] | str = st.text_input("Enter comma-seperated foods. Example) 고기,냉면")
+foods = [food.strip() for food in foods.split(",")]
+restaurant = st.text_input("Enter restaurant(Deprecated)")
 
 if not st.button("Write!"):
     st.stop()
-if not (title and restaurant and review and max_length):
+if not (title and review and max_length and keywords and foods):
     st.warning("Fill all inputs")
     st.stop()
 
-post: str = write_post(title=title, restaurant=restaurant, review=review, max_length=max_length)
-hashtags: list[str] = write_hashtags(post=post)
+post_guide = PostGuide(
+    title=title,
+    review=review,
+    max_length=max_length,
+    keywords=keywords,
+    foods=foods,
+    restaurant=restaurant,
+)
+if not restaurant:
+    restaurant = find_restaurant(title)
+
+post_guide = post_guide.with_restaurant(restaurant)
+post: str = write_post(post_guide=post_guide)
+hashtags: list[str] = write_hashtags(post=post, post_guide=post_guide)
 
 result = f"""
 # {title}
