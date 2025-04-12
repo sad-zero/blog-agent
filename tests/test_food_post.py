5ba0f16f470a8de17df03576f2b084a5ee4ab8f4
sad-zero <zeroro.yun@gmail.com>
2025-04-13 08:29:31 +0900

    test: update post test

--- tests/test_food_post.py
+++ tests/test_food_post.py
@@ -3,36 +3,40 @@ import pytest
 from blog_agent.agent import PostGuide, find_restaurant, write_hashtags, write_post
 
 
-def test_write_post():
-    # given
-    post_guide = PostGuide(
+@pytest.fixture
+def post_guide() -> PostGuide:
+    return PostGuide(
         title='서울의 "소고기 천국" 다녀왔어요!',
-        review="정말 맛있는 소고기를 직접 구워주셨습니다!",
+        review="""
+직접 구워주시는 소고기는 촉촉함의 극치였어요!
+한번만 먹을 수 없는 그 맛..
+정말 열번 가도 또 가고 싶습니다.
+방문 적극 추천해요!
+대단히 맛있던 냉면은 소고기와 함께 먹을때 최고의 궁합을 자랑했어요
+        """.strip(),
         max_length=1500,
-        keywords=["숯불", "직화", "맛집"],
+        keywords=["서울맛집", "소고기대장", "직화구이"],
         foods=["소고기", "냉면"],
     )
+
+
+def test_write_post(post_guide):
+    # given
+    post_length_offset = 100
     # when
     restaurant: str = find_restaurant(title=post_guide.title)
     post_guide = post_guide.with_restaurant(restaurant)
     post: str = write_post(post_guide=post_guide)
     # then
     assert post.startswith("안녕하세요, 오늘 소개해드릴 곳은 소고기 천국입니다!")
-    assert len(post.split()) <= post_guide.max_length
+    assert post_guide.max_length - post_length_offset <= len(post.split()) <= post_guide.max_length + post_length_offset
     chunks: list[str] = [post[i : i + 300] for i in range(0, len(post), 300)]
     for chunk in chunks:
         assert any(keyword in chunk for keyword in post_guide.keywords)
 
 
 @pytest.fixture
-def post_info():
-    post_guide = PostGuide(
-        title='서울의 "소고기 천국" 다녀왔어요!',
-        review="정말 맛있는 소고기를 직접 구워주셨습니다!",
-        max_length=1500,
-        keywords=["숯불", "직화", "맛집"],
-        foods=["소고기", "냉면"],
-    )
+def post_info(post_guide):
     # when
     restaurant: str = find_restaurant(post_guide.title)
     post_guide = post_guide.with_restaurant(restaurant)
